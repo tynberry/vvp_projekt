bf015eab8a491a3b38d143c7e001f5b7e781098b
Tynberry <28360155+tynberry@users.noreply.github.com>
2025-04-14 19:19:46 +0200

    feat: generace mandelbrot

--- mandelbrot/__init__.py
+++ mandelbrot/__init__.py
@@ -4,3 +4,7 @@ Knihovna generace a vizualizace Mandelbrot a Julia fraktálů.
 Generace používá numpy a Numbu.
 Vizualizace se provádí v PyGame.
 """
+
+from .generace import *
+
+__all__ = ["mandelbrot", "julia_set"]
--- mandelbrot/generace.py
+++ mandelbrot/generace.py
@@ -1,9 +1,25 @@
 import numba 
 import numpy as np
 
+@numba.vectorize([numba.int64(numba.complex128, numba.complex128, numba.int32)])
+def count_iters(state: numba.complex128, c: numba.complex128, max_iter: numba.int32) -> numba.int64:
+    """
+    Spočítá počet operací než hodnota vyběhne ven. 
+
+    state je počáteční hodnota.
+    c je číslo c přičítané každou iteraci
+    max_iter je maximální počet iterací 
+    """
+    for i in range(max_iter):
+        state **= 2
+        state += c 
+        if np.abs(state) > 2:
+            return i+1
+    return max_iter
+
+
 #@numba.jit(signature="int64[:,:](float64,float64,float64,float64,int64,int64)",
 #           nopython=True, locals={"divergence", numba.np_ar})
-@numba.njit
 def mandelbrot(x_min:float, x_max:float, y_min:float, y_max:float, cells: int, max_iter: int) -> np.array:
     """
     Vygeneruj Mandelbrotovu množinu.
@@ -17,18 +33,13 @@ def mandelbrot(x_min:float, x_max:float, y_min:float, y_max:float, cells: int, m
     #divergenční matice 
     divergence = np.zeros((cells,cells), dtype=np.int32)
     #matice stavu 
-    state = np.zeros((cells,cells), dtype=np.complex128)
+    #state = np.zeros((cells,cells), dtype=np.complex128)
     #matice C komplexních čísel   
     x = np.linspace(x_min, x_max, cells, dtype=np.complex128).reshape((1,cells))
     y = np.linspace(y_min, y_max, cells, dtype=np.complex128).reshape((cells,1)) * 1j
     c_matrix = x + y,
-    #udělej iterace 
-    for i in range(max_iter):
-        #udělej jednu iteaci 
-        state **= 2
-        state += c_matrix
-        #přičti iteraci 
-        divergence[np.abs(state) <= 2] += 1
+    #spočti iterace 
+    divergence = count_iters(0, c_matrix, max_iter)
     #vrať počet operací
     return divergence
 
